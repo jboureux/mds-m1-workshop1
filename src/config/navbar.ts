e823c28823e669592932ca11a315cc322a651fdd
Jérémy Boureux <jeremyboureux@gmail.com>
2024-09-18 10:24:23 +0200

    chore: create navbar config

---
+++ src/config/navbar.ts
@@ -0,0 +1,10 @@
+export const navbar = [
+    {
+        name: "Personnalisation",
+        href: "/custom",
+    },
+    {
+        name: "Administration",
+        href: "/admin/dashboard",
+    },
+];

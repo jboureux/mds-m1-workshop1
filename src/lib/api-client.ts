0f77348d1f21cc2d5a3b225e55352d7755e54700
Jérémy Boureux <jeremyboureux@gmail.com>
2024-11-26 11:58:47 +0100

    docker dev OK

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -4,34 +4,39 @@ const methods = z.enum(["GET", "POST", "PATCH", "PUT", "DELETE"]);
 const allowedContentTypes = z.enum(["application/json"]);
 
 export async function fetchFromAPI<T>(
-    route: string,
-    method: z.infer<typeof methods>,
-    body?: unknown,
-    options?: {
-        headers?: {
-            contentType?: z.infer<typeof allowedContentTypes>;
-        };
-    }
+  route: string,
+  method: z.infer<typeof methods>,
+  body?: unknown,
+  options?: {
+    headers?: {
+      contentType?: z.infer<typeof allowedContentTypes>;
+    };
+  }
 ): Promise<{ data?: T; error?: Error }> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${route}`, {
-        method: method,
-        body: body ? JSON.stringify(body) : null,
-        headers: {
-            "Content-Type":
-                options && options.headers && options.headers?.contentType
-                    ? options.headers.contentType
-                    : "application/json",
-        },
-        cache: "no-store",
-    });
+  const apiUrl =
+    typeof window === "undefined"
+      ? process.env.NEXT_SERVER_API_URL
+      : process.env.NEXT_PUBLIC_API_URL;
 
-    const responseData = await response.json();
+  const response = await fetch(`${apiUrl}${route}`, {
+    method: method,
+    body: body ? JSON.stringify(body) : null,
+    headers: {
+      "Content-Type":
+        options && options.headers && options.headers?.contentType
+          ? options.headers.contentType
+          : "application/json",
+    },
+    cache: "no-store",
+  });
 
-    const result: { data?: T; error?: Error } = {};
-    if (response.ok) {
-        result.data = responseData;
-    } else {
-        result.error = responseData.message;
-    }
-    return result;
+  const responseData = await response.json();
+
+  const result: { data?: T; error?: Error } = {};
+  if (response.ok) {
+    result.data = responseData;
+  } else {
+    result.error = responseData.message;
+  }
+  return result;
 }
